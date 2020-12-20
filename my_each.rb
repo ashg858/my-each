e4c4e62a88701a09c35b0d7f95eb6ab34aeab8f2
ashg858 <aguidry27598@gmail.com>
2020-12-19 20:12:34 -0600

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+def my_each
+  collection = [1, 2, 3, 4]
+  while collection.length < 4 do
+    yield (collection)
+  end
+end
+
+def my_each(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    return collection [i] if yield(collection)
+    i = i + 1
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
